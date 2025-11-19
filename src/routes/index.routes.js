0fbe7c79b3c129cb5f9e8edd4540898ea0838bb8
estefSolis <estefanymichelle2@gmail.com>
2025-11-18 22:08:16 -0600

    Actualizacion de index.routes.js

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -2,15 +2,19 @@ import { Router } from 'express';
 import { healthController } from '../controllers/health.controller.js';
 import estacionRouter from './estacion.routes.js';
 import eventoRouter from './evento.routes.js';
-import grupoImg1Router from './gruposI1.routes.js';
-import grupoImg2Router from './gruposI2.routes.js';
+import gruposRouter from './grupos.routes.js';
+import imagenesReliRouter from './imagenReligiosa.routes.js';
+import recorridoRouter from './recorrido.routes.js'
+
 
 const router = Router();
 
 router.get('/health', healthController);
 router.use('/evento', eventoRouter);
 router.use('/estacion', estacionRouter);
-router.use('/grupoImg1', grupoImg1Router);
-router.use('/grupoImg2', grupoImg2Router);
+router.use('/grupos', gruposRouter);
+router.use('/imgReligiosa', imagenesReliRouter);
+router.use('/recorrido', recorridoRouter);
+
 
 export default router;
